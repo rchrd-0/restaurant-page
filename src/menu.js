0e3c551c0bce04d079e79b839cb79b3d3da62330
richard <richard.ytli+f@gmail.com>
2022-05-11 11:05:45 +0800

    Write function to insert menu content

--- src/menu.js
+++ src/menu.js
@@ -13,14 +13,52 @@ function loadMenu() {
     const sectionHeader = document.createElement('h2');
     sectionHeader.textContent = section.toUpperCase();
     container.appendChild(sectionHeader);
+
+    let content = getContent(container);
+    content.forEach(item => container.appendChild(item));
+    
     menu.appendChild(container);
   })
 
   return menu;
 }
 
-function insertContent(section) {
+function getContent(div) {
+  const allContent = {
+    meat: [
+      'Hong Kong-style sand ginger chicken, house-made scallion sauce',
+      'Lamb chops, laccha onion',
+      'Roast duck, hoisin sauce, steamed pancakes',
+      'Australian Wagyu skirt steak, thick-cut rice noodles, seaweed butter, preserved lemon'
+    ],
+    seafood: [
+      'Tiger prawn noodles, soy butter sauce, prawn oil, dried shrimp roe',
+      'Eel on crispy sushi rice & pickles',
+      'Cobia, sapphire chutney, lime caviar',
+      'Grilled lobster, green mango, pineapple & lime vinaigrette'
+    ],
+    salad: [
+      'Shredded Mushrooms, cold noodles, cucumber, chilli oil, sesame dressing',
+      'Shredded chicken salad, cabbage, rau ram, prawn chips, pickles',
+      'Pork belly, pickles, sichuan peppercorn oil'
+    ],
+    sides: [
+      'Chicken wings, lemongrass, red chilli, salt, pepper',
+      'Crispy eggplant, mixed herbs, ginger glaze',
+      'Shrimp tacos, lime garlic cabbage, mango salsa, cilantro, onions'
+    ]
+  }
+  const keys = Object.keys(allContent);
+  let section = keys.filter(key => key === div.dataset.menu).pop();
+
+  let pArray = new Array;
+  allContent[section].forEach(item => {
+    const p = document.createElement('p');
+    p.textContent = item;
+    pArray.push(p);
+  })
   
+  return pArray;
 }
 
 export default loadMenu;
